4437b59a89d54cbd2b4ee2678568d822008bff21
Ritielen <ritielen.r.silva@gmail.com>
2026-02-24 17:39:15 -0300

    Remove comments from index.js
    Removed comments for EJS configuration, static files, and example route.

--- index.js
+++ index.js
@@ -2,14 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// Configurar EJS
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Servir arquivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Rota exemplo
 app.get('/', (req, res) => {
   res.render('login/home'); 
 });
